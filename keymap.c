fd72d0aec88b47b71857a56781723e488382a17b
Maksim Vinogradov <vinmax96@gmail.com>
2026-07-20 12:47:17 +0400

    propagate shift to other layers

--- keymap.c
+++ keymap.c
@@ -37,24 +37,24 @@ LAYOUT(
                    KC_F18, KC_LALT, KC_LGUI, MO(SYM), KC_SPC,    SFT_ENT,  MO(NAV), KC_RGUI, KC_RALT, KC_HYPR
 ),
 LAYOUT(
-    KC_NO, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    KC_NO, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,                        MS_BTN5, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_F12,
-    KC_NO, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,                        MS_BTN4, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO,
-    KC_NO, KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_P0,     KC_NO,   KC_NO,   KC_NO,   KC_BSPC, KC_NO,   KC_DEL,  KC_NO,   KC_NO,
-                    KC_TRNS, KC_TRNS, KC_TRNS, MO(MEDIA), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    KC_NO,   KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,                     MS_BTN5, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_F12,
+    KC_NO,   KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,                     MS_BTN4, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO,
+    KC_TRNS, KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_P0,     KC_NO,   KC_NO,   KC_NO,   KC_BSPC, KC_NO,   KC_DEL,  KC_NO,   KC_NO,
+                      KC_TRNS, KC_TRNS, KC_TRNS, MO(MEDIA), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 LAYOUT(
-    KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,      KC_NO,                       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
-    KC_NO, KC_NO, KC_NO,   S(KC_5), S(KC_6),    S(KC_4),                     S(KC_9),    S(KC_0),    KC_EQL,     S(KC_EQL),  S(KC_BSLS), KC_GRV,
-    KC_NO, KC_NO, KC_NO,   S(KC_8), S(KC_SLSH), S(KC_1),                     KC_LBRC,    KC_RBRC,    KC_MINS,    S(KC_MINS), KC_SCLN,    S(KC_SCLN),
-    KC_NO, KC_NO, KC_NO,   S(KC_7), S(KC_2),    S(KC_3),   KC_NO,   KC_NO,   S(KC_LBRC), S(KC_RBRC), S(KC_COMM), S(KC_DOT),  KC_SLSH,    KC_BSLS,
-                  KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS, MO(MEDIA),  KC_TRNS,    KC_TRNS,    KC_TRNS
+    KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,      KC_NO,                     KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+    KC_NO,   KC_NO, KC_NO,   S(KC_5), S(KC_6),    S(KC_4),                   S(KC_9),    S(KC_0),    KC_EQL,     S(KC_EQL),  S(KC_BSLS), KC_GRV,
+    KC_NO,   KC_NO, KC_NO,   S(KC_8), S(KC_SLSH), S(KC_1),                   KC_LBRC,    KC_RBRC,    KC_MINS,    S(KC_MINS), KC_SCLN,    S(KC_SCLN),
+    KC_TRNS, KC_NO, KC_NO,   S(KC_7), S(KC_2),    S(KC_3), KC_NO,   KC_NO,   S(KC_LBRC), S(KC_RBRC), S(KC_COMM), S(KC_DOT),  KC_SLSH,    KC_BSLS,
+                    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, MO(MEDIA),  KC_TRNS,    KC_TRNS,    KC_TRNS
 ),
 LAYOUT(
-    RM_TOGG, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_BRID, KC_NO,   KC_BRIU, KC_NO,   KC_NO,
-    KC_NO,   RM_SPDU, RM_VALU, RM_SATU, RM_HUEU, RM_NEXT,                   KC_NO,   KC_VOLD, KC_MUTE, KC_VOLU, KC_NO,   KC_NO,
-    KC_NO,   RM_SPDD, RM_VALD, RM_SATD, RM_HUED, RM_PREV,                   KC_NO,   KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,   KC_NO,
-    AG_TOGG, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MCTL, KC_NO,   KC_LPAD, KC_NO,
+    RM_TOGG, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_BRID, KC_NO,   KC_BRIU, KC_NO, KC_NO,
+    KC_NO,   RM_SPDU, RM_VALU, RM_SATU, RM_HUEU, RM_NEXT,                   KC_NO,   KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO,
+    KC_NO,   RM_SPDD, RM_VALD, RM_SATD, RM_HUED, RM_PREV,                   KC_NO,   KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO,
+    AG_TOGG, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MCTL, KC_NO,   KC_LPAD, KC_NO, KC_NO,
                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 )
 };
