0d268b24899f006aefe61505c0c253dd8c66c29c
Maksim Vinogradov <vinmax96@gmail.com>
2026-07-23 11:52:31 +0400

    remap symbol layer, add tab lt. add shift space

--- keymap.c
+++ keymap.c
@@ -1,18 +1,3 @@
-/* Copyright 2023 Brian Low
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
 #include "transactions.h"
@@ -27,30 +12,33 @@
 #define LCG_LEFT LCG(KC_LEFT)
 #define RCG_RGHT LCG(KC_RGHT)
 #define SFT_ENT LSFT_T(KC_ENT)
+#define SFT_SPC LSFT_T(KC_SPC)
 #define BSPC_LT LT(NAV, KC_BSPC)
 #define DEL_LT LT(MEDIA, KC_DEL)
+#define TAB_L1 LT(SYM, KC_TAB)
+#define TAB_L2 LT(MEDIA, KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 LAYOUT(
-    KC_GRV,  KC_1, KC_2,   KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-    KC_TAB,  KC_Q, KC_W,   KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,
-    CTL_ESC, KC_A, KC_S,   KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z, KC_X,   KC_C,    KC_V,    KC_B,    LCG_LEFT,  RCG_RGHT, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RBRC,
-                   KC_F18, KC_LALT, KC_LGUI, MO(SYM), KC_SPC,    SFT_ENT,  BSPC_LT, KC_RGUI, KC_RALT, KC_HYPR
+    KC_GRV,  KC_1, KC_2,   KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
+    KC_TAB,  KC_Q, KC_W,   KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,
+    CTL_ESC, KC_A, KC_S,   KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z, KC_X,   KC_C,    KC_V,    KC_B,   LCG_LEFT,  RCG_RGHT, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RBRC,
+                   KC_F18, KC_LALT, KC_LGUI, TAB_L1, SFT_SPC,   SFT_ENT,  BSPC_LT, KC_RGUI, KC_RALT, KC_HYPR
 ),
 LAYOUT(
-    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    KC_NO,   KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,                     MS_BTN5, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_F12,
-    KC_NO,   KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,                     MS_BTN4, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO,
-    KC_TRNS, KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_P0,     KC_NO,   KC_NO,   KC_NO,   KC_BSPC, KC_NO,   KC_DEL,  KC_NO,   KC_NO,
-                      KC_TRNS, KC_TRNS, KC_TRNS, MO(MEDIA), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    KC_NO,   KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,                   MS_BTN5, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_F12,
+    KC_TRNS, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,                   MS_BTN4, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO,
+    KC_TRNS, KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_P0,   KC_NO,   KC_NO,   KC_INS,  KC_BSPC, KC_DEL,  KC_NO,   KC_NO,   KC_NO,
+                      KC_TRNS, KC_TRNS, KC_TRNS, TAB_L2,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 LAYOUT(
-    KC_NO,   KC_NO, KC_NO,   KC_NO,      KC_NO,   KC_NO,                        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
-    KC_NO,   KC_NO, KC_QUOT, S(KC_QUOT), S(KC_6), S(KC_4),                      S(KC_9),    S(KC_0),    KC_EQL,     S(KC_EQL),  S(KC_BSLS), KC_GRV,
-    KC_NO,   KC_NO, S(KC_2), S(KC_7),    S(KC_1), S(KC_SLSH),                   KC_LBRC,    KC_RBRC,    KC_MINS,    S(KC_MINS), KC_SCLN,    S(KC_SCLN),
-    KC_TRNS, KC_NO, S(KC_5), S(KC_3),    S(KC_7), S(KC_8),    KC_NO,   KC_NO,   S(KC_LBRC), S(KC_RBRC), S(KC_COMM), S(KC_DOT),  KC_SLSH,    KC_BSLS,
-                    KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, DEL_LT,     KC_TRNS,    KC_TRNS,    KC_TRNS
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,                        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+    KC_NO,   KC_NO,   S(KC_2), S(KC_3),    S(KC_6), S(KC_4),                      S(KC_LBRC), S(KC_RBRC), KC_EQL,     S(KC_EQL),  KC_TRNS,    KC_TRNS,
+    KC_TRNS, S(KC_5), KC_QUOT, S(KC_QUOT), S(KC_1), S(KC_SLSH),                   S(KC_9),    S(KC_0),    KC_MINS,    S(KC_MINS), KC_TRNS,    KC_TRNS,
+    KC_TRNS, KC_NO,   KC_BSLS, S(KC_BSLS), S(KC_7), S(KC_8),    KC_NO,   KC_NO,   KC_LBRC,    KC_RBRC,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+                      KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, DEL_LT,     KC_TRNS,    KC_TRNS,    KC_TRNS
 ),
 LAYOUT(
     RM_TOGG, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_BRID, KC_NO,   KC_BRIU, KC_NO, KC_NO,
