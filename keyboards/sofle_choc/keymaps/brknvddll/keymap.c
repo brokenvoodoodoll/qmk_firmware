9ee57de8bba94e0b6de5b78c5df75829e963b2c1
Maksim Vinogradov <vinmax96@gmail.com>
2026-09-14 12:13:05 +0400

    fix del trns

--- keyboards/sofle_choc/keymaps/brknvddll/keymap.c
+++ keyboards/sofle_choc/keymaps/brknvddll/keymap.c
@@ -63,7 +63,7 @@ LAYOUT(
     KC_TRNS, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     KC_TRNS, KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_NO,    KC_7,    KC_8,    KC_9,    KC_NO,   KC_NO,
     KC_TRNS, KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,                    KC_NO,    KC_4,    KC_5,    KC_6,    KC_NO,   KC_GRV,
-    KC_DEL,  KC_NO, KC_NO,   KC_NO,   KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_0,     KC_1,    KC_2,    KC_3,    KC_BSLS, KC_NO,
+    KC_TRNS, KC_NO, KC_NO,   KC_NO,   KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_0,     KC_1,    KC_2,    KC_3,    KC_BSLS, KC_NO,
                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BSPC_LT2, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 LAYOUT(
